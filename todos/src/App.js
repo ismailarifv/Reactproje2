d3b46a56f178114c6fb4b5620d1a180b300da11d
Ismail Arif Gulec <i.arifgulec8110@gmail.com>
2023-03-07 16:21:44 +0300

    react-proje2

---
+++ todos/src/App.js
@@ -0,0 +1,12 @@
+import Contacts from "./components/Contacts"
+import './App.css';
+
+function App() {
+  return (
+    <div className="todoapp">
+       <Contacts/>
+    </div>
+  );
+}
+
+export default App;
